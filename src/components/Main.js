ad27fc30ecab1875a43ebc70dbf15875871c9d6b
mackwick <mackenziewicker@gmail.com>
2024-01-26 20:01:07 -0500

    basic styling of Projects component done, but will likely add more

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,13 @@
+import Projects from "./Projects";
+
 // the component function
 const Main = (props) => {
   //The Components Returned JSX
-  return <div className="Main"> This is Main</div>;
+  return (
+    <div className="Main">
+      <Projects />
+    </div>
+  );
 };
 
 // export the component
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,34 @@
 // the component function
 const Projects = (props) => {
   //The Components Returned JSX
-  return <div className="Projects"> This is Projects</div>;
+  return (
+    <div className="Projects">
+      <div className="example">
+        <h2>Potluck</h2>
+        <img src="https://i.imgur.com/Lehmmpz.png" />
+        <div className="links">
+          <a href="https://potluck.onrender.com/">Live Site |</a>
+          <a href="https://github.com/mackwick/potluck">| GitHub</a>
+        </div>
+        <p>
+          Technologies Used: JavaScript, MongoDB, Mongoose, Express, Node, EJS,
+          CSS, Google Fonts
+        </p>
+      </div>
+      <div className="example">
+        <h2>What Are We Drinking Tonight?</h2>
+        <img src="https://i.imgur.com/ylXzr6s.png" />
+        <div className="links">
+          <a href="https://ga-unit1-project.vercel.app/">Live Site |</a>
+          <a href="https://github.com/mackwick/GA-Unit1_Project">| GitHub</a>
+        </div>
+        <p>
+          Technologies Used: Javascript, jQuery, HTML, CSS, The Cocktail DB API,
+          Google Fonts
+        </p>
+      </div>
+    </div>
+  );
 };
 
 // export the component
