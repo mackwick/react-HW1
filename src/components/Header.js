8c580905c1a13a34167abf9b2322453e9c3121db
mackwick <mackenziewicker@gmail.com>
2024-01-26 19:18:43 -0500

    added and styled Header component

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+// the component function
+const Header = (props) => {
+  //The Components Returned JSX
+  return (
+    <div className="Header">
+      <h1>Mackenzie Wicker: Portfolio</h1>
+      <nav>
+        <a href="https://github.com/mackwick">My GitHub</a>
+
+        <a href="https://www.linkedin.com/in/mackenziewicker/">My LinkedIn</a>
+      </nav>
+    </div>
+  );
+};
+
+// export the component
+export default Header;
---
+++ src/components/Projects.js
@@ -0,0 +1,8 @@
+// the component function
+const Projects = (props) => {
+  //The Components Returned JSX
+  return <div className="Projects"> This is Projects</div>;
+};
+
+// export the component
+export default Projects;
